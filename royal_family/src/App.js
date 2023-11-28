088181917859cf5d9a19c3d1fc5f4341a49555c5
Egor Pankratiev <112758007+Riborok@users.noreply.github.com>
2023-11-28 16:59:18 +0300

    added project core

--- royal_family/src/App.js
+++ royal_family/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import About from "./pages/About";
+import Navbar from "./components/UI/navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <Switch>
+                <Route path = '/about'> <About/> </Route>
+                <Redirect to = '/about'/>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ royal_family/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            // TODO Описание портала (Стас)
+
+            // TODO Деяетель дня (Яна)
+
+            // TODO Наша крутая команда (Артур)
+        </div>
+    );
+};
+
+export default About;
