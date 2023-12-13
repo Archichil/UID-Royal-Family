edfc8f96f0d417b268d91daaca38ad2d4558e20e
Ferret451 <stas.gabrus@gmail.com>
2023-12-13 16:17:53 +0300

    added description

---
+++ src/components/Description.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Container from "@mui/material/Container";
+import Divider from '@mui/material/Divider';
+import '../css/Descriptoin.css';
+
+const Description = () => {
+    return (
+        <Container className='container' maxWidth="fm">
+            <Stack direction="row" className="mainStack">
+                <Stack direction="column" className="sideStack">
+                    <p className='mainText'> О ЧЕМ </p>
+                    <Divider sx={{ width: '80%' }} color="black"/>
+                    <p className='sideText'>
+                        На сайте вы сможете познакомистя и узнать что-то новое о
+                        голландской королевской семьей и о ее членах в частности
+                    </p>
+                </Stack>
+                <Stack direction="column" className="sideStack">
+                    <p className='mainText'>  ЦЕЛИ </p>
+                    <Divider sx={{ width: '80%' }} color="black"/>
+                    <p className='sideText'>
+                        Наш портал ставит своей целью просвятить общество,
+                        что есть и иные королевские семьи кроме британской
+                    </p>
+                </Stack>
+                <Stack direction="column" className="sideStack">
+                    <p className='mainText'> КОМУ </p>
+                    <Divider sx={{ width: '80%' }} color="black"/>
+                    <p className='sideText'>
+                        Кроме историков и 'прошаренных' людей сайт подойдет
+                        и для обычных любознательных пользователей
+                    </p>
+                </Stack>
+            </Stack>
+        </Container>
+
+    );
+};
+
+export default Description;
