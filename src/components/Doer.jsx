8acc4d0f1d001bf5fae95aac8b57adafac63f3be
Raw <zhuk-2005.03.04@mail.ru>
2023-12-16 16:17:48 +0300

    Doer section (some adaptability)

--- src/components/Doer.jsx
+++ src/components/Doer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Stack from "@mui/material/Stack";
 import '../css/doer_section.css';
 
 const Doer = () => {
@@ -8,7 +9,13 @@ const Doer = () => {
     return (
         <div className="doer_section">
             <p className="sectionName">Королевский представитель дня</p>
-            <div className="descriptDoer">
+            {/*<div className="descriptDoer">*/}
+            <Stack
+                 direction={{ xs: 'column', sm: 'row' }}
+                 spacing={5}
+                 alignItems='center'
+                 justifyContent="center"
+                 className="descriptDoer">
                 <div className="photoDoerEdging">
                     <img className="photoDoer" src="https://www.tvr.by/upload/iblock/a13/b6k91b5ew8wt3me9iravpe7ty20tml63.jpg" ></img>
                     {/*<img className="photoDoer" src="{data.imageUrl}" alt="{data.namePerson}"></img>*/}
@@ -17,15 +24,16 @@ const Doer = () => {
                     <p className="nameZone">Карл |||</p>
                     {/*<p className="nameZone">{data.namePerson}</p>*/}
                     <p className="shortDescript">Карл III (1716–1788), также известный как Карл III Бурбон,
-                                                 был королём Испании с 1759 по 1788 год. Он был представителем династии
-                                                 Бурбонов и вторым сыном Филиппа V. Во время своего правления Карл III
-                                                 провёл ряд реформ и модернизаций в Испании.</p>
+                        был королём Испании с 1759 по 1788 год. Он был представителем династии
+                        Бурбонов и вторым сыном Филиппа V. Во время своего правления Карл III
+                        провёл ряд реформ и модернизаций в Испании.</p>
                     {/*<p className="shortDescript">{data.descriptionPerson}</p>*/}
                     <button className="btnDoer" onClick={handleButtonClick}>
                         Просмотр
                     </button>
                 </div>
-            </div>
+            </Stack>
+            {/*</div>*/}
         </div>
     );
 };
