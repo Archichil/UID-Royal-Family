1a34421a18ea7f2434b87b8c508fd8cb58f24c57
Archichil <kuhockovolec@gmail.com>
2023-12-17 12:04:22 +0300

    Fixes - Changed FamilyMembers ids to proper one - Fixed misspelling in json

--- src/pages/FamilyMembers.jsx
+++ src/pages/FamilyMembers.jsx
@@ -10,15 +10,15 @@ const FamilyMembers = () => {
     const {t} = useTranslation();
 
     const members = [
-        { id: 1, name: t('familyMembersName.willem'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/king-willem-alexander-2020-01-3-2.jpg'},
-        { id: 2, name: t('familyMembersName.maxima'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/queen-maxima-2020-01-3-2.jpg'},
-        { id: 3, name: t('familyMembersName.beatrix'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/portretfoto-s/prinses-beatrix/princess-beatrix-2020-1.jpg'},
-        { id: 4, name: t('familyMembersName.orange'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/princesss-of-orange-2021-03.jpg'},
-        { id: 5, name: t('familyMembersName.alex'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/2023-princess-alexia-03-small.jpg'},
-        { id: 6, name: t('familyMembersName.ariane'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/2020-princess-ariane-1200x800.jpg'},
-        { id: 7, name: t('familyMembersName.const'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/portretfoto-s/prins-constantijn/prince-constantijn-03-2020.jpg'},
-        { id: 8, name: t('familyMembersName.laurien'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/portretfoto-s/prinses-laurentien/princess-laurentien-03-2020.jpg'},
-        { id: 9, name: t('familyMembersName.margriet'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/princess-margriet-2020-01-3-2.jpg'}
+        { id: 'king-willem-alexander', name: t('familyMembersName.willem'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/king-willem-alexander-2020-01-3-2.jpg'},
+        { id: 'queen-maxima', name: t('familyMembersName.maxima'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/queen-maxima-2020-01-3-2.jpg'},
+        { id: 'princess-beatrix', name: t('familyMembersName.beatrix'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/portretfoto-s/prinses-beatrix/princess-beatrix-2020-1.jpg'},
+        { id: 'the-princess-of-orange', name: t('familyMembersName.orange'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/princesss-of-orange-2021-03.jpg'},
+        { id: 'princess-alexia', name: t('familyMembersName.alex'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/2023-princess-alexia-03-small.jpg'},
+        { id: 'princess-ariane', name: t('familyMembersName.ariane'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/2020-princess-ariane-1200x800.jpg'},
+        { id: 'prince-constantijn', name: t('familyMembersName.const'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/portretfoto-s/prins-constantijn/prince-constantijn-03-2020.jpg'},
+        { id: 'princess-laurentien', name: t('familyMembersName.laurien'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/portretfoto-s/prinses-laurentien/princess-laurentien-03-2020.jpg'},
+        { id: 'princess-margriet', name: t('familyMembersName.margriet'), src: 'https://www.royal-house.nl/binaries/medium/content/gallery/royalhouse/content-afbeeldingen/fotonavigatie-450x300/portretfotos/princess-margriet-2020-01-3-2.jpg'}
     ];
 
     const filteredData = members.filter((item) =>
