63d70b6e9d887fdca35a52a92c0efb91331781bb
Archichil <kuhockovolec@gmail.com>
2023-12-18 14:00:13 +0300

    Created GoogleMaps.jsx component to display google maps on the member's page

---
+++ src/components/GoogleMaps.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../css/GoogleMaps.css'
+
+const GoogleMaps = ({link}) => {
+    return (
+        <iframe
+            className = "MapContainer"
+            style={{
+                marginTop: "1em",
+                border: '0',
+                boxShadow: '-moz-initial',
+                WebkitBoxShadow: '5px 6px 50px grey'
+            }}
+            width= "100%"
+            src = {link}
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+        >
+        </iframe>
+    );
+};
+
+export default GoogleMaps;
