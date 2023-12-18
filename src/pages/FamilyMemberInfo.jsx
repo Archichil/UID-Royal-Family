520c80bbf810fb4a72d5172012e2cf702f7f81a2
Archichil <kuhockovolec@gmail.com>
2023-12-18 15:07:47 +0300

    Completed json for 2 languages + completed FamilyMemberInfo.jsx (TODO Refactoring)

--- src/pages/FamilyMemberInfo.jsx
+++ src/pages/FamilyMemberInfo.jsx
@@ -1,49 +1,109 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 import {useTranslation} from "react-i18next";
+import YouTube from '../components/YouTube';
 import {Chrono} from "react-chrono";
-
+import {
+    Container,
+    Typography,
+    Grid,
+    Card,
+    CardMedia,
+} from '@mui/material';
+import GallerySlider from "../components/GallerySlider";
+import GoogleMaps from "../components/GoogleMaps";
 
 const FamilyMemberInfo = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth', });
     const id = useParams().id;
     const {t} = useTranslation();
 
     return (
         <div>
-            <div style={{width: "100%", height: "600px"}}>
-                <Chrono
-                    items={t(`familyMembers.${id}.timelineItems`, { returnObjects: true })}
-                    mode="VERTICAL_ALTERNATING"
-                    allowDynamicUpdate={true}
+            <Container>
+                <Typography variant="h1" align="center" sx={{ my: 4 }}>
+                    {t(`familyMembers.${id}.name`)}
+                </Typography>
+                <Grid container spacing={4}>
+                    <Grid item xs={12} md={6}>
+                        <Card>
+                            <CardMedia component="img" height="500" image={t(`familyMembers.${id}.images`, {returnObjects: true})[0]} alt={t(`familyMembers.${id}.name`)}/>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h4" sx={{mb: 2}} style={{whiteSpace: "pre-wrap"}}>
+                            {t('lifeYears') + t(`familyMembers.${id}.birthdayDate`) + '\n'}
+                            {t('age') + t(`familyMembers.${id}.age`) + '\n'}
+                            {t('annotation') + t(`familyMembers.${id}.description`)}
+                        </Typography>
+                    </Grid>
 
-                    cardHeight={100}
-                    mediaHeight={100}
-                    contentDetailsHeight={80}
-                    readMore={true}
+                    <div style={{width: "100%", marginTop:"70px"}}>
+                        <Typography variant="h2" align="center" sx={{ my: 4 }}>
+                            {t(`timeline`)}
+                        </Typography>
+                        <div style={{width: "100%", height: "800px"}}>
+                            <Chrono
+                                items={t(`familyMembers.${id}.timelineItems`, {returnObjects: true})}
+                                mode="VERTICAL_ALTERNATING"
+                                allowDynamicUpdate={true}
 
-                    fontSizes={{
-                        cardSubtitle: '1.6rem',
-                        title: '1.9rem',
-                        cardTitle: '1.7rem'
-                    }}
+                                cardHeight={100}
+                                mediaHeight={100}
+                                contentDetailsHeight={80}
+                                readMore={true}
 
-                    theme={{
-                        primary: '#212121',
-                        secondary: '',
-                        cardBgColor: 'floralwhite',
-                        titleColor: '#212121',
-                        titleColorActive: '#212121',
-                        cardTitleColor: '#212121',
-                    }}
+                                fontSizes={{
+                                    cardSubtitle: '1.6rem',
+                                    title: '1.9rem',
+                                    cardTitle: '1.7rem'
+                                }}
 
-                    classNames={{
-                        cardSubTitle: 'my-card-subtitle',
-                        cardText: 'my-card-text',
-                        cardTitle: 'my-card-title',
-                        title: 'my-title',
-                    }}
-                />
-            </div>
+                                theme={{
+                                    primary: '#212121',
+                                    secondary: '',
+                                    cardBgColor: 'floralwhite',
+                                    titleColor: '#212121',
+                                    titleColorActive: '#212121',
+                                    cardTitleColor: '#212121',
+                                }}
+
+                                classNames={{
+                                    cardSubTitle: 'my-card-subtitle',
+                                    cardText: 'my-card-text',
+                                    cardTitle: 'my-card-title',
+                                    title: 'my-title',
+                                }}
+                            />
+                        </div>
+                    </div>
+
+                    <Grid item xs={12} style={{marginTop: "70px"}}>
+                        <Typography variant="h2" align="center" sx={{mb: 2}}>
+                            {t('photoGallery')}
+                        </Typography>
+                        <GallerySlider
+                            items={t(`familyMembers.${id}.images`, {returnObjects: true})}
+                        />
+                    </Grid>
+                    <Grid item xs={12} style={{marginTop: "70px"}}>
+                        <Typography variant="h2" align="center" sx={{mb: 2}}>
+                            {t('youtube')}
+                        </Typography>
+                        <YouTube
+                            link={t(`familyMembers.${id}.youtube`)}
+                        />
+                    </Grid>
+                    <Grid item xs={12} style={{marginTop: "70px"}}>
+                        <Typography variant="h2" align="center" sx={{mb: 2}}>
+                            {t('googleMap')}
+                        </Typography>
+                        <GoogleMaps
+                            link={t(`familyMembers.${id}.googleMap`)}
+                        />
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     );
 };
